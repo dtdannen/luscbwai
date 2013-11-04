514b78798515bcbc29dbd732a5097856b04ef8a2
Steven Stinson <srs514@lehigh.edu>
2013-11-04 14:34:52 -0500

    Now no longer references ColorNodes directly

--- LUSCBWAI/UAlbertaBot/VisualStudio/GoalAdvisor.cpp
+++ LUSCBWAI/UAlbertaBot/VisualStudio/GoalAdvisor.cpp
@@ -2,18 +2,18 @@
 
 class ColorNode;
 
-ColorNode* getSmallest(std::list<ColorNode*>,std::map<int,double>);
+int getSmallest(std::list<ColorNode*>,std::map<int,double>);
 double distanceBetweenCenters(int,int);
-ColorNode* smallestNonGreenNode(std::map<int,double>);
+int smallestNonGreenNode(std::map<int,double>);
 
-ColorNode* GoalAdvisor::getGoalRegion() {
+int GoalAdvisor::getGoalRegion() {
 	return goalNode;
 }
 
 GoalAdvisor::GoalAdvisor() {
 	BWAPI::Player* p = BWAPI::Game::self();
 	BWAPI::Position* startLocation = p.getStartLocation();
-	originNode = ColorGraph::Instance().getNodeById(ColorGraph::Instance().getNodeAtPosition(startLocation));
+	originNode = ColorGraph::Instance().getNodeAtPosition(startLocation);
 	GoalAdvisor::update();
 }
 
@@ -24,23 +24,23 @@ void GoalAdvisor::update() {
 	std::map<int,double> distance_map = * new std::map<int,double>();
 	std::map<int,bool> visited_map = * new std::map<int,bool>();
 
-	std::list<ColorNode*> q = *new std::list<ColorNode*>;
+	std::list<int> q = *new std::list<int>;
 
 	distance_map[originNode->getId()]=0;
 	q.push_back(originNode);
 
 	while(!q.empty()) {
-		ColorNode * current = getSmallest(q,distance_map);
+		int current = getSmallest(q,distance_map);
 		q.remove(current);
 		visited_map[current]=true;
 
-		std::list<int> neighbors = current->getNeighbors();
+		std::list<int> neighbors = ColroGraph::Instance().getNodeNeighbors(current);
 		for (int i = 0; i < neighbors.size(); i++) {
-			double alt = distance_map[current] + distanceBetweenCenters(current->getId(),neighbors[i]);
+			double alt = distance_map[current] + distanceBetweenCenters(current,neighbors[i]);
 			//if distance map contains i and is bigger than the current calculated distance, and we have not visited i yet
 			if (distance_map.find(i)!=distance_map.end() && alt < distance_map[i] && !visited_map[i]) {
 				distance_map[i] = alt;
-				q.push_back(ColorGraph::Instance().getNodeById(neighbors[i]));
+				q.push_back(neighbors[i]);
 			}
 		}
 
@@ -53,18 +53,18 @@ void GoalAdvisor::update() {
 	delete &q;
 }
 
-ColorNode* getSmallest(std::list<ColorNode*> list, std::map<int,double> map) {
-	std::list<ColorNode*>::const_iterator it;
+int getSmallest(std::list<int> list, std::map<int,double> map) {
+	std::list<int>::const_iterator it;
 
 	double smallest_distance = DBL_MAX;
-	ColorNode* smallest = NULL;
+	int smallest = -1;
 
 	for (it = list.begin(); it!=list.end(); ++it) {
-		int id = (*it)->getId();
+		int id = *it;
 		double dist = map[id];
 		if (dist<smallest_distance) {
 			smallest_distance=dist;
-			smallest = *it;
+			smallest = id;
 		}
 
 	}
@@ -73,11 +73,8 @@ ColorNode* getSmallest(std::list<ColorNode*> list, std::map<int,double> map) {
 }
 
 double distanceBetweenCenters(int one, int two) {
-	ColorNode* one = ColorGraph::Instance().getNodeById(one);
-	ColorNode* two = ColorGraph::Instance().getNodeById(two);
-
-	BWAPI::Position oneCenter = one.getCenter();
-	BWAPI::Position twoCenter = two.getCenter();
+	BWAPI::Position oneCenter = ColorGraph::Instance().getNodeCenter(one);
+	BWAPI::Position twoCenter = ColorGraph::Instance().getNodeCenter(two);
 
 	int* oneCoords = {oneCenter.x(), oneCenter.y()};
 	int* twoCoords = {twoCenter.x(), twoCenter.y()};
@@ -85,20 +82,20 @@ double distanceBetweenCenters(int one, int two) {
 	return sqrt(pow(twoCoords[0]-oneCoords[0],2)+pow(twoCoords[1]-oneCoords[1],2));
 }
 
-ColorNode* smallestNonGreenNode(std::map<int,double> map) {
-	std::list<ColorNode*> nonGreenNodes = * new std::list<ColorNode*>();
+int smallestNonGreenNode(std::map<int,double> map) {
+	std::list<int> nonGreenNodes = * new std::list<int>();
 
 	std::map<int,double>::const_iterator it;
 	
 	for(it = map.begin(), it!=map.end(); ++it) {
-		ColorNode* current = ColorGraph::Instance().getNodeById(std::get<0>(*it)); //std::pair is weird
-		if (current->getColor()!=NodeColor::GREEN) {
+		int current = std::get<0>(*it); //std::pair is weird
+		if (ColorGraph::Instance().getNodeColor(current)!=NodeColor::GREEN) {
 			nonGreenNodes.push_back(current);
 		}
 	}
 
 	//instead of directly returning, just place it in a variable so we can clear the memory used for nonGreenNodes
-	ColorNode* result = getSmallest(nonGreenNodes,map);
+	int result = getSmallest(nonGreenNodes,map);
 	delete &nonGreenNodes;
 	return result;
 }
