1ae9f2db0dd233e9da6c3fbb20f580341ae21315
pinieb <petebiencourt@gmail.com>
2014-02-16 23:47:41 -0500

    first frontier changes

---
+++ LUSCBWAI/UAlbertaBot/VisualStudio/FrontierAdvisor.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <list>
+#include <BWTA.h>
+#include "ColorGraph.h"
+
+class FrontierAdvisor
+{
+private:
+	FrontierAdvisor(void);
+	~FrontierAdvisor(void);
+	double calculateNodeValue(int id);
+
+	std::list<int> frontier; // holds the nodes that we consider to be part of our frontier
+	std::list<int> controlled; // holds the nodes behind the frontier
+
+public:
+	static FrontierAdvisor &	Instance();
+	int getNextNodeId();
+	void addNodeToFrontier(int id);
+	void recalculateFrontier();
+};
+
